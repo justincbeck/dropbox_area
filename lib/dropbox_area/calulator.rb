afd236e8986428ba3eb292792075dcc41eb8d763
Justin Beck <justinbeck@mac.com>
2011-08-18 10:27:41 -0400

    Added a solution for a single file - Easy, of course

--- lib/dropbox_area/calulator.rb
+++ lib/dropbox_area/calulator.rb
@@ -1,6 +1,20 @@
 module DropboxArea
   class Calculator
-    def initialize
+    def initialize output
+      @output = output
+      @error = $stderr
+    end
+
+    def execute num_files, dims
+      area = dims[0][0] * dims[0][1]
+      @output.puts area
+      @error.puts draw num_files, dims
+    end
+
+    private
+
+    def draw num_files, dims
+      "Not Implemented"
     end
   end
 end
--- spec/dropbox_area/calculator_spec.rb
+++ spec/dropbox_area/calculator_spec.rb
@@ -2,8 +2,15 @@ require File.expand_path(File.dirname(__FILE__)) + '/../spec_helper'
 
 module DropboxArea
   describe Calculator do
-    describe "execute" do
-      it "should do something"
+    let(:output) { double('output').as_null_object }
+    let(:calc) { Calculator.new(output) }
+
+    describe "execute with a single file" do
+      it "should return the area of the file" do
+        file_dim = [2, 2]
+        output.should_receive(:puts).with(4)
+        calc.execute(1, [file_dim])
+      end
     end
   end
 end
